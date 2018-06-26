cb3cddf20ab6a5ac33499516fd024eb03a1e3a5e
lochga01 <36273830+lochga01@users.noreply.github.com>
2018-06-26 13:23:16 -0500

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,36 +1,85 @@
 import os
+from dotenv import load_dotenv
 import re
+from flask import abort, Flask, jsonify, request, redirect, render_template
+from slackclient import SlackClient
 
-from flask import abort, Flask, jsonify, request
+# initialize dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__),'.env')
+load_dotenv(dotenv_path)
 
 
 app = Flask(__name__)
 
+# Variables
+# APPLICATION_TOKEN
+# CLIENT_ID
+# CLIENT_SECRET
+
 
 def is_request_valid(request):
-    is_token_valid = request.form['token'] == os.environ['VERIFICATION_TOKEN']
-    is_team_id_valid = request.form['team_id'] == os.environ['TEAM_ID']
+    is_token_valid = False
 
-    return is_token_valid and is_team_id_valid
+    if request.form['token'] == os.environ['APPLICATION_TOKEN']:
+        is_token_valid = True
+
+
+    return is_token_valid
 
 def parse(txt):
+    if txt == "help":
+        buildStr = "This will return a completed link to that ticket in KBOX. Neat, huh?"
+        return buildStr
+
     txt = txt.split()
     buildStr = ""
+
     for word in txt:
-        if re.search(".*\d{5,7}",tryStr):
-            buildStr = "<https://helpdesk.luther.edu/adminui/ticket.php?ID=" + word + '>'
+        if re.search(".*\d{5,7}",word):
+            buildStr = "<https://help.luther.edu/adminui/ticket.php?ID=" + word + '>'
             return buildStr
-        else:
-            buildStr = "No ticket found."
+
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/support')
+def support():
+    return render_template('support.html')
+
 
 @app.route('/ticket', methods=['POST'])
 def hello_there():
     if not is_request_valid(request):
         abort(400)
-    text = request.form.get('text',None)
+    text = request.form.get('text')
     resText = parse(text)
 
     return jsonify({'response_type':'in_channel','text':resText})
+
+@app.route('/authorize')
+def redir():
+    return redirect('https://slack.com/oauth/authorize?client_id=375533386721.377769613264&scope=commands', code=302)
+
+@app.route('/oauth/authorized', methods=['GET', 'POST'])
+def calledBack():
+    a_code = request.args['code']
+    sc = SlackClient("")
+
+    auth_response = sc.api_call(
+                                "oauth.access",
+                                client_id = os.environ['CLIENT_ID'],
+                                client_secret = os.environ['CLIENT_SECRET'],
+                                code=a_code)
+
+    os.environ["SLACK_USER_TOKEN"] = auth_response['access_token']
+    #os.environ["SLACK_BOT_TOKEN"] = auth_response['bot']['bot_access-token']
+    return 'Auth completed.'
+
 
 if __name__ == '__main__':
     app.run(debug=True,port=5001)
