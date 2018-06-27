6d028c3d9306156dbd006044e3fad577b575d03e
lochga01 <36273830+lochga01@users.noreply.github.com>
2018-06-26 23:44:16 -0500

    Added comments.

--- routes.py
+++ routes.py
@@ -16,29 +16,29 @@ app = Flask(__name__)
 # CLIENT_ID
 # CLIENT_SECRET
 
-
+# Checks token from request against app_token
 def is_request_valid(request):
     is_token_valid = False
-
     if request.form['token'] == os.environ['APPLICATION_TOKEN']:
         is_token_valid = True
-
-
     return is_token_valid
 
+# Main parser. Takes str input, uses regex to find if word contains ticket #.
 def parse(txt):
+    # if the only text is help, will return a string.
     if txt == "help":
         buildStr = "This will return a completed link to that ticket in KBOX. Neat, huh?"
         return buildStr
-
     txt = txt.split()
     buildStr = ""
-
     for word in txt:
         if re.search(".*\d{5,7}",word):
             buildStr = "<https://help.luther.edu/adminui/ticket.php?ID=" + word + '>'
             return buildStr
 
+###############
+## Web pages ##
+###############
 @app.route('/privacy')
 def privacy():
     return render_template('privacy.html')
@@ -51,20 +51,23 @@ def index():
 def support():
     return render_template('support.html')
 
-
+# Slash command, needs POST method
 @app.route('/ticket', methods=['POST'])
 def hello_there():
+    # Checks validity of request
     if not is_request_valid(request):
         abort(400)
     text = request.form.get('text')
     resText = parse(text)
-
+    # Returned link is pasted into the channel.
     return jsonify({'response_type':'in_channel','text':resText})
 
+# Redirection to slack Oauth
 @app.route('/authorize')
 def redir():
     return redirect('https://slack.com/oauth/authorize?client_id='+ os.environ['CLIENT_ID']+'&scope=commands', code=302)
 
+# Callback URL; returns whether or not app was successfully added. 
 @app.route('/oauth/authorized', methods=['GET', 'POST'])
 def calledBack():
     a_code = request.args['code']
